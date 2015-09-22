9bb91dd6ccf8ae0258453dc5faeb74dcdbee8f52
Pavlo Romanenko <Pavlo_Romanenko@epam.com>
2015-09-22 15:14:50 +0300

    Scripts updated

--- runImport.bat
+++ runImport.bat
@@ -1,2 +1,2 @@
-%JAVA_HOME%\bin\java -classpath D:\gigaspaces-xap-premium-10.0.1-ga\lib\required\*;target\* com.gigaspaces.tools.importexport.SpaceDataImportExportMain -i -l 127.0.0.1 -s space
+%JAVA_HOME%\bin\java -classpath D:\gigaspaces-xap-premium-10.0.1-ga\lib\required\*;lib\* com.gigaspaces.tools.importexport.SpaceDataImportExportMain -i -l 127.0.0.1 -s space
 
