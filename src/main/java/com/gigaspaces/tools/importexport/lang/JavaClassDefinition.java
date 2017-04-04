668ed721a883604ce5340b3ab147c0fe14f03ec3
Benoit Lacelle <benoit.lacelle@solven.eu>
2017-04-04 12:09:44 +0200

    Handle POJO with setters instead of fields

--- src/main/java/com/gigaspaces/tools/importexport/lang/JavaClassDefinition.java
+++ src/main/java/com/gigaspaces/tools/importexport/lang/JavaClassDefinition.java
@@ -1,12 +1,17 @@
 package com.gigaspaces.tools.importexport.lang;
 
-import com.j_spaces.core.client.SQLQuery;
-
 import java.io.Serializable;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.gigaspaces.internal.utils.ReflectionUtils;
+import com.gigaspaces.internal.utils.ReflectionUtils.FieldCallback;
+import com.gigaspaces.internal.utils.ReflectionUtils.FieldFilter;
+import com.j_spaces.core.client.SQLQuery;
 
 /**
  * Created by skyler on 12/1/2015.
@@ -14,6 +19,7 @@ import java.util.Map;
 public class JavaClassDefinition extends SpaceClassDefinition implements Serializable {
     private static final long serialVersionUID = 4910571443452951970L;
     private transient Field routingField;
+	private transient Method routingGetter;
 
     public JavaClassDefinition(String className, VersionSafeDescriptor typeDescriptor){
         super(className, typeDescriptor);
@@ -24,54 +30,105 @@ public class JavaClassDefinition extends SpaceClassDefinition implements Seriali
         return new SQLQuery(Class.forName(this.className), "");
     }
 
-    @Override
-    public HashMap<String, Object> toMap(Object instance) throws ClassNotFoundException, IllegalAccessException {
-        HashMap<String, Object> output = new HashMap<String, Object>();
+	@Override
+	public HashMap<String, Object> toMap(final Object instance) throws ClassNotFoundException, IllegalAccessException {
+		final HashMap<String, Object> output = new HashMap<String, Object>();
 
-        Class<?> aClass = Class.forName(className);
-        Field[] fields = aClass.getDeclaredFields();
+		Class<?> aClass = Class.forName(className);
 
-        for(Field currentField : fields){
-            if(!canBeSet(currentField)) continue;
+		// Iterate through all fields, including parent classes
+		ReflectionUtils.doWithFields(aClass, new FieldCallback() {
 
-            currentField.setAccessible(true);
-            output.put(currentField.getName(), currentField.get(instance));
-        }
+			@Override
+			public void doWith(Field currentField) throws IllegalArgumentException, IllegalAccessException {
+				ReflectionUtils.makeAccessible(currentField);
 
-        return output;
-    }
+				output.put(currentField.getName(), currentField.get(instance));
+			}
+		}, new FieldFilter() {
+
+			@Override
+			public boolean matches(Field field) {
+				return !Modifier.isStatic(field.getModifiers());
+			}
+		});
+
+		return output;
+	}
 
     private boolean canBeSet(Field currentField) {
         return !Modifier.isFinal(currentField.getModifiers());
     }
 
     @Override
-    public Object getRoutingValue(Object instance) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
-        String routingPropertyName = typeDescriptor.getRoutingPropertyName();
+	public Object getRoutingValue(Object instance)
+			throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+		String routingPropertyName = typeDescriptor.getRoutingPropertyName();
 
-        if(this.routingField == null) {
-            Class<?> aClass = Class.forName(className);
-            routingField = aClass.getDeclaredField(routingPropertyName);
-            routingField.setAccessible(true);
-        }
+		if (this.routingField == null && this.routingGetter == null) {
+			Class<?> aClass = Class.forName(className);
 
-        return routingField.get(instance);
-    }
+			try {
+				routingField = aClass.getDeclaredField(routingPropertyName);
+				routingField.setAccessible(true);
+			} catch (NoSuchFieldException e) {
+				// The routingPropertyName is not a field. It may be a JavaBean setter
 
-    @Override
-    public Object toInstance(HashMap<String, Object> asMap) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchFieldException {
-        Class<?> aClass = Class.forName(className);
-        Object output = aClass.newInstance();
+				// http://stackoverflow.com/questions/5503412/java-standard-library-to-convert-field-name-firstname-to-accessor-method-na
+				try {
+					String methodName = "get" + routingPropertyName.substring(0, 1).toUpperCase()
+							+ routingPropertyName.substring(1);
+					routingGetter = org.springframework.util.ReflectionUtils.findMethod(aClass, methodName);
+				} catch (SecurityException e1) {
+					throw new RuntimeException(e1);
+				}
+			}
+		}
 
-        for(Map.Entry<String, Object> property : asMap.entrySet()){
-            Field field = aClass.getDeclaredField(property.getKey());
+		if (routingField != null) {
+			return routingField.get(instance);
+		} else if (routingGetter != null) {
+			try {
+				return routingGetter.invoke(instance);
+			} catch (IllegalArgumentException e) {
+				throw new RuntimeException(e);
+			} catch (InvocationTargetException e) {
+				throw new RuntimeException(e);
+			}
+		} else {
+			throw new RuntimeException("Found n oway to introspect " + className + " for field " + routingPropertyName);
+		}
+	}
 
-            if(!canBeSet(field)) continue;
+	@Override
+	public Object toInstance(HashMap<String, Object> asMap)
+			throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchFieldException {
+		Class<?> aClass = Class.forName(className);
+		final Object output = aClass.newInstance();
 
-            field.setAccessible(true);
-            field.set(output, property.getValue());
-        }
+		for (final Map.Entry<String, Object> property : asMap.entrySet()) {
+			// Iterate through all fields, including parent classes
+			ReflectionUtils.doWithFields(aClass, new FieldCallback() {
 
-        return output;
-    }
+				@Override
+				public void doWith(Field currentField) throws IllegalArgumentException, IllegalAccessException {
+					ReflectionUtils.makeAccessible(currentField);
+
+					try {
+						currentField.set(output, property.getValue());
+					} catch (Throwable e) {
+						e.printStackTrace();
+					}
+				}
+			}, new FieldFilter() {
+
+				@Override
+				public boolean matches(Field field) {
+					return field.getName().equals(property.getKey()) && !Modifier.isStatic(field.getModifiers());
+				}
+			});
+		}
+
+		return output;
+	}
 }
